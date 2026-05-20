787b3ba1219bb7d0dfbd0e49b0ae2f8982d5fee7
danilonishimoto <daniyuzo.nishimoto@gmail.com>
2026-05-20 20:48:10 -0300

    fix: fallback

--- src/components/ForecastCard/index.jsx
+++ src/components/ForecastCard/index.jsx
@@ -8,7 +8,7 @@ const ForecastCard = ({ weather, weekday }) => {
         <p>{formatWeekday(weekday)}</p>
         <p>{weather?.date}</p>
       </div>
-      <img className={styles.weatherIcon} src={`./icons-weather/${weather?.condition}.svg`}/>
+      <img className={styles.weatherIcon} src={weather?.condition_slug ? `./icons-weather/${weather?.condition_slug}.svg` : `./icons-weather/fallback.svg}`}/>
       <p className={styles.temp}>{weather?.min}/{weather?.max}°</p>
     </div>
   )
